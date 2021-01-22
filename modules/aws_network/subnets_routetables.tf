68dac653f8e957b5e9fcd1b6376353810f07f3dd
Alex Yaremchuk <77787272+yaremchuk3@users.noreply.github.com>
2021-01-22 14:46:52 +0200

    Add files via upload

---
+++ modules/aws_network/subnets_routetables.tf
@@ -0,0 +1,43 @@
+resource "aws_subnet" "publicSubnets" {
+  vpc_id = aws_vpc.myVpc.id
+  count = length(var.public_subnet_cidr_blocks)
+  availability_zone = element(local.zones, count.index)
+  cidr_block = element(var.public_subnet_cidr_blocks, count.index)
+  map_public_ip_on_launch = true
+  tags = merge(var.common_tags, {Name = "Public-Subnet-${count.index +1}-${var.env}", Env = var.env})
+}
+
+resource "aws_subnet" "privateSubnets" {
+  vpc_id = aws_vpc.myVpc.id
+  count = length(var.private_subnet_cidr_blocks)
+  availability_zone = element(local.zones, count.index)
+  cidr_block = element(var.private_subnet_cidr_blocks, count.index)
+  depends_on = [aws_vpc_ipv4_cidr_block_association.myVpcSecond_cidr]
+  tags = merge(var.common_tags, {Name = "Private-Subnet-${count.index +1}-${var.env}", Env = var.env})
+}
+
+resource "aws_route_table" "myRouteTablePublicSubnets" {
+  count = length(aws_subnet.publicSubnets[*])
+  vpc_id = aws_vpc.myVpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myIGW.id
+  }
+  tags = merge(var.common_tags, {Name = "RouteTablePublicSubnets-${var.env}", Env = var.env})
+}
+
+resource "aws_route_table" "myRouteTablePrivateSubnets" {
+  count = length(aws_subnet.privateSubnets[*])
+  vpc_id = aws_vpc.myVpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = element(aws_nat_gateway.myNatGW[*].id, count.index)
+  }
+  tags = merge(var.common_tags, {Name = "RouteTablePrivateSubnets-${var.env}", Env = var.env})
+}
+
+resource "aws_route_table_association" "myPublicSubnets" {
+  count = length(aws_subnet.publicSubnets[*].id)
+  route_table_id = element(aws_route_table.myRouteTablePublicSubnets[*].id, count.index)
+  subnet_id = element(aws_subnet.publicSubnets[*].id, count.index)
+}
---
+++ modules/aws_network/vpc_sg.tf
@@ -0,0 +1,31 @@
+resource "aws_vpc" "myVpc" {
+  cidr_block = var.vpc_cidr_block
+  tags = merge(var.common_tags, {Name = "myVpc-${var.env}", Env = var.env})
+}
+
+resource "aws_vpc_ipv4_cidr_block_association" "myVpcSecond_cidr" {
+  count = length(var.vpc_additional_cidr_block)
+  cidr_block = element(var.vpc_additional_cidr_block, count.index)
+  vpc_id = aws_vpc.myVpc.id
+}
+
+resource "aws_security_group" "myDynamicSG" {
+  name_prefix = "${var.env}-SGDynamic-"
+  vpc_id = aws_vpc.myVpc.id
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  dynamic "ingress" {
+    for_each = var.sg_open_ports
+    content {
+      from_port = ingress.value
+      to_port = ingress.value
+      protocol = var.sg_open_protocol
+      cidr_blocks = var.elb_info.elb_ingress_cidr
+    }
+  }
+  tags = merge(var.common_tags, {Name = "myDynamicSG-${var.env}", Env = var.env})
+}
