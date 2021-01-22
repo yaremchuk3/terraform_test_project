c43482708cea96472210e6e6d4a4845d74788e0e
Alex Yaremchuk <77787272+yaremchuk3@users.noreply.github.com>
2021-01-22 14:48:42 +0200

    Delete modules directory

--- modules/eip.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_eip" "myIPsNatGW"{
-  count = length(aws_subnet.privateSubnets[*].id)
-  tags = merge(var.common_tags, {Name = "EIP_for_NatGW-${count.index}-${var.env}", Env = var.env})
-}
